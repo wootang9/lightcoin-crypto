e718ba850caab49c84af8e8c387845700787f168
Brendan Woo <woo.brendan@gmail.com>
2022-01-21 20:17:43 +0000

    completion of lightcoin assignment

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+  get balance() {
+    let balance = 0;
+    for (let exchange of this.transactions) {
+      balance += exchange.value;
+    }
+    return balance;
+  }
 
-class Withdrawal {
+  addTransaction(transaction) {
+    this.transactions.push(transaction)
+  }
+}
 
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
+}
 
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -Math.abs(this.amount);
+  }
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+  isAllowed() {
+    return true;
+  }
 }
 
 
@@ -18,12 +55,25 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const myAccount = new Account ('Snow-Patrol');
+console.log('Starting Balace: ', myAccount.balance);
+console.log('Attepming to withdraw even $1 should fail...');
+const t1 = new Withdrawal(1, myAccount);
+console.log('commit result: ',t1.commit());
+console.log('Account balance: ', myAccount.balance);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Depositing should succeed...');
+const t2 = new Deposit(9.99, myAccount);
+console.log('commit result: ',t2.commit());
+console.log('Account balance: ', myAccount.balance);
 
-console.log('Balance:', balance);
+console.log('Attepming to withdraw $9.99 should pass...');
+const t3 = new Withdrawal(9.99, myAccount);
+console.log('commit result: ',t3.commit());
+
+console.log('Ending Account balance: ', myAccount.balance);
+
+
+console.log('Account tranaction history', myAccount.transactions);
+
+// console.log('Transaction 3:', t3);
